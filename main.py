e947c72da165d8747500c47bbdaa2d5195ea3d9a
Michael Sanders <michaelsand155@gmail.com>
2018-11-05 20:39:27 +1000

    on_ready

---
+++ main.py
@@ -0,0 +1,39 @@
+import discord
+from discord.ext import commands
+
+def getToken():
+    try:
+        tokenFile = open("token.ini", "r")
+        tokenData = tokenFile.readline()
+    except(FileNotFoundError):
+        tokenFile = open("token.ini", "w")
+        print("No token file found, creating one. Please enter the bot token in 'token.ini' \n")
+        input("Press enter to continue")
+        tokenFile.close()
+        return None
+    tokenFile.close()
+    if tokenData == "":
+        print("Token File empty, please enter in Bot Token in 'token.ini'")
+        input("Press enter to continue\n")
+        return None
+    else:
+        return tokenData
+
+TOKEN = None
+
+while TOKEN is None:
+    TOKEN = getToken()
+
+#Hang's here until token file is confirmed
+
+client = commands.Bot(command_prefix = "squirt ") 
+
+@client.event 
+async def on_ready():
+    print("Bot Connected and is ready to recieve commands")
+
+
+
+
+client.run(TOKEN)
+
